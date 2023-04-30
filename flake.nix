078fbc486667c171bb43a252f37db28059972a7c
nbe <aleisa.naser@gmail.com>
2023-04-30 05:30:42 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Playwright with Chromium";
+
+  inputs = {
+    nixpkgs.url = "github:thenbe/nixpkgs/playwright-driver-1.33.0";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        wrapped-playwright = pkgs.runCommand "wrapped-playwright" {buildInputs = [pkgs.makeWrapper];} ''
+          mkdir -p "$out/bin"
+          makeWrapper "${pkgs.playwright-driver}/bin/playwright" "$out/bin/playwright" \
+            --set PLAYWRIGHT_BROWSERS_PATH "${pkgs.playwright-driver.browsers}" \
+            --set PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD 1 \
+        '';
+      in {
+        packages = {
+          playwright = wrapped-playwright;
+          default = wrapped-playwright;
+        };
+      }
+    );
+}
