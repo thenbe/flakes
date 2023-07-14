198d8b2da4df65ac519365436ab06ec0cde2bde6
nbe <aleisa.naser@gmail.com>
2023-07-14 05:32:10 +0300

    add dt

--- flake.nix
+++ flake.nix
@@ -36,6 +36,27 @@
         packages = {
           playwright-driver = wrapped-playwright-driver;
           # default = wrapped-playwright-driver;
+
+          dt = pkgs.stdenv.mkDerivation rec {
+            pname = "dt";
+            version = "1.1.0";
+            src = pkgs.fetchurl {
+              url = "https://github.com/booniepepper/dt/releases/download/v${version}/x86_64-linux-gnu.tgz";
+              sha256 = "sha256-ciU194O3ppG9IjgxY3HQ2+Fdr7QszxONWXNcN/tPFxA=";
+            };
+            unpackPhase = ''
+              tar -xzf $src
+            '';
+            dontBuild = true;
+            installPhase = ''
+              mkdir -p $out/bin
+              cp x86_64-linux-gnu/bin/dt $out/bin/dt
+            '';
+            meta = {
+              description = "dt binary";
+              homepage = "https://github.com/booniepepper/dt";
+            };
+          };
         };
 
         # export config for nixOS (includes package and sets env vars)
