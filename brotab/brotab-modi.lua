84b10b50c237fe8f36987b3c8cb497f2023bbe3a
nbe <aleisa.naser@gmail.com>
2023-08-08 08:04:16 +0300

    feat: add brotob rofi tab switcher

---
+++ brotab/brotab-modi.lua
@@ -0,0 +1,33 @@
+#!/usr/bin/env lua
+
+local bp = require("lua-shepi")
+local rofi_markup = print("\0markup-rows\x1ftrue\n")
+
+local function GetTabs(sin, sout, serr)
+	local delim = print("\x00delim\x1f\x0f")
+	local markup_s =
+		'%s\n<span foreground="#6B838E">%s\n</span><span foreground="#44555D"><small><i>%s</i></small></span>\x0f'
+	local pipe_in = sin:read("a")
+	for line in pipe_in:gmatch("([^\n]*)\n") do
+		local id, title, url = line:match("([^\t]+)\t([^\t]+)\t([^\t]+)")
+		local result = string.format(markup_s, title, url:match("https?://[www%.]*(.*)"), id)
+		local prune_s = result:gsub("&", "&amp;")
+		sout:write(prune_s)
+	end
+end
+
+args = { ... }
+local pipe = bp.bt("list") | bp.tac("-s", "\n") | bp.fun(GetTabs)
+if not args[1] then
+	io.write(pipe())
+else
+	local id = args[1]:match("<i>(.-)</i>")
+	-- title is whatever is before the first <span> tag
+	local title = args[1]:match("^(.-)<span")
+	-- local url = args[1]:match("<small><i>(.-)</i></small>")
+
+	-- select the tab in the browser window
+	os.execute(string.format("bt activate %s", id))
+	-- focus the browser window (we might need a tiny delay here to ensure the title is consistently set)
+	os.execute(string.format("wmctrl -a %s", title))
+end
---
+++ brotab/flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:thenbe/nixpkgs/lua-shepi-1.4.1"; # TODO: only for lua-shepi
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        luaEnv = pkgs.lua5_3.withPackages (ps: with ps; [
+          luaposix
+          lua-shepi
+        ]);
+        brotab-modi = pkgs.stdenv.mkDerivation {
+          name = "brotab-modi";
+          buildInputs = [ pkgs.brotab ];
+          propagatedBuildInputs = [ luaEnv pkgs.lua5_3 ];
+          src = ./brotab-modi.lua;
+          dontUnpack = true;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/bin/brotab-modi
+            chmod +x $out/bin/brotab-modi
+          '';
+        };
+      in
+      {
+        packages = {
+          default = brotab-modi;
+        };
+        apps.brotab-modi = {
+          type = "app";
+          program = "${brotab-modi}/bin/brotab-modi";
+        };
+      }
+    );
+}
