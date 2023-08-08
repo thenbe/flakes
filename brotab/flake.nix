8338347ac272221d5ee4185eb1ff308bd179c3d7
nbe <aleisa.naser@gmail.com>
2023-08-08 10:11:17 +0300

    fix: use a different way to provide brotab as a runtime dependency

--- brotab/flake.nix
+++ brotab/flake.nix
@@ -16,13 +16,14 @@
         ]);
         brotab-modi = pkgs.stdenv.mkDerivation {
           name = "brotab-modi";
-          buildInputs = [ pkgs.brotab luaEnv ];
+          buildInputs = [ pkgs.makeWrapper pkgs.brotab luaEnv ];
           src = ./brotab-modi.lua;
           dontUnpack = true;
           installPhase = ''
             mkdir -p $out/bin
             cp $src $out/bin/brotab-modi.lua
             chmod +x $out/bin/brotab-modi.lua
+            wrapProgram $out/bin/brotab-modi.lua --prefix PATH : ${pkgs.brotab}/bin
           '';
         };
       in
