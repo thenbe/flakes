4470f4a1fd0bfc4f246ee1061a2f9ff4e409c40a
nbe <aleisa.naser@gmail.com>
2023-09-10 18:10:40 +0300

    crawley: 1.6.8 -> 1.6.14

--- crawley/default.nix
+++ crawley/default.nix
@@ -1,16 +1,16 @@
 { pkgs }:
-pkgs.buildGoModule rec {
+pkgs.buildGo121Module rec {
   pname = "crawley";
-  version = "1.6.8";
+  version = "1.6.14";
 
   src = pkgs.fetchFromGitHub {
     owner = "s0rg";
     repo = "crawley";
     rev = "v${version}";
-    hash = "sha256-PSWpqPCk/qJrP79dWGkipSgQerD5Y9jIWdXAKy33zO8=";
+    hash = "sha256-XnDo0rC8sdjGDFPS6WIQucpl9m7SPnAc4SPHtn01Vqk=";
   };
 
-  vendorSha256 = "sha256-W3pXmrHhZDGjqA0K/3ohdgUZKOClh4qNC3chZKums2k=";
+  vendorSha256 = "sha256-cbrXYf+s94ZKwlR44+J5oufPWPUC6yOhl6K19q0bun8=";
   subPackages = [ "cmd/crawley" ];
 
   meta = {
